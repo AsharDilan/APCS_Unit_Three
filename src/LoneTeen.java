2be85fe95ca83051cc25a41d3187df83722fa563
AsharDilan <ashardi@ssfs.org>
2021-10-28 09:00:48 -0400

    1

--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -2,4 +2,13 @@ public class LoneTeen {
 
     // In the space below, write the loneTeen method. Make sure the signature matches
     // the one given in the instructions.
-}
+    public static Boolean loneTeen(int num1, int num2) {
+
+        if (num1 <= 19 && num1 >= 13 && num2 < 13 || num2 > 19)
+
+            return true;
+
+        else if (num2>=13)
+
+        return false;
+    }
--- src/SlopeOfALine.java
+++ src/SlopeOfALine.java
@@ -4,6 +4,7 @@ public class SlopeOfALine {
 
     /**
      * Calculates slope (y2-y1)/(x2-x1) of a line as long as x2 - x1 != 0
+     *
      * @param x1 - x-coordinate of first point
      * @param y1 - y-coordinate of first point
      * @param x2 - x-coordinate of second point
@@ -13,32 +14,36 @@ public class SlopeOfALine {
     public static String slope(int x1, int y1, int x2, int y2) {
         // update the line below to return a properly formatted string
         return "";
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter x1:");
+        int xuno = scan.nextInt();
+        System.out.print("Enter y1:");
+        int yuno = scan.nextInt();
+        System.out.print("Enter x2:");
+        int xdos = scan.nextInt();
+        System.out.print("Enter y2:");
+        int ydos = scan.nextInt();
+
+        int y = (ydos - yuno);
+        int x = (xdos - xuno);
+
+        if (x != 0)
+            return ("The line defined by the points " + "(" + x1 + "," + y1 + ") " + "and " + "(" + x2 + "," + y2 + ") " + "is a vertical line and the slope is " + y / x + ".");
+        else
+            return ("The line defined by the points " + "(" + x1 + "," + y1 + ") " + "and " + "(" + x2 + "," + y2 + ") " + "is a vertical line and the slope is undefined.");
+    }
+}
+}
+        {
+
     }
 
     public static void main(String[] args) {
         // Get input from the user (you will need to create a Scanner object)
         // Call and print the result of the slope method as shown in the instructions.
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter x1:");
-        int x1 = scan.nextInt();
-        System.out.print("Enter y1:");
-        int y1 = scan.nextInt();
-        System.out.print("Enter x2:");
-        int x2 = scan.nextInt();
-        System.out.print("Enter y2:");
-        int y2 = scan.nextInt();
-
-        int y = (y2-y1);
-        int x = (x2-x1);
-
-
-        if (x!=0){
-            System.out.println("The line defined by the points " + "(" + x1 + "," + y1 + ") " + "and" + "(" + x2 + "," + y2 + ") " + "is a vertical line and the slope is " + y/x + ".");
 
         }
-        else{
-            System.out.println("The line defined by the points " + "(" + x1 + "," + y1 + ") " + "and" + "(" + x2 + "," + y2 + ") " + "is a vertical line and the slope is undefined.");
-        }
-    }
 
-}
+
+
+
